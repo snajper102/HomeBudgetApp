fee174a4934dcd9acc91aab3027971e29087c74c
Bartosz Zontek <34723838+snajper102@users.noreply.github.com>
2024-06-11 17:59:12 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeBudgetApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(new Views.MainPage());
+    }
+}
